aadad19e22e0a83797cf6f7b81eb6113b8a3381b
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-13 22:11:49 -0300

    CTD

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,16 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", boolean=False)
+
 
 @auth.route('/logout')
 def logout():
     return "<p> log out</p>"
+
 
 @auth.route("/sign-up")
 def sign_up():
